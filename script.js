9c4e19f3e5e177a500e1259563ab7e654cd9fed5
Jeajoe11 <148391783+Jeajoe11@users.noreply.github.com>
2024-06-13 17:23:14 -0400

    ss

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const Add1Cookie = document.getElementById("Add1Cookie");
 const Add5Cookie = document.getElementById("Add5Cookie");
 const Add10Cookie = document.getElementById("Add10Cookie");
 const Add50Cookie = document.getElementById("Add50Cookie");
+const Add150Cookie = document.getElementById("Add150Cookie");
 
 //AUTO ADDITION COOKIES
 const AutoAdd1Cookie = document.getElementById("AutoAdd1Cookie");
 const AutoAdd5Cookie = document.getElementById("AutoAdd5Cookie");
 const AutoAdd10Cookie = document.getElementById("AutoAdd10Cookie");
 const AutoAdd50Cookie = document.getElementById("AutoAdd50Cookie");
+const AutoAdd150Cookie = document.getElementById("AutoAdd150Cookie");
 
 
 //EXTRA
@@ -28,13 +30,14 @@ let Add1CookiePrice = 12;
 let Add5CookiePrice = 104;
 let Add10CookiePrice = 269;
 let Add50CookiePrice = 1200;
+let Add150CookiePrice = 2169;
 
 //AUTO ADDITION COOKIES PRICES
 let AutoAdd1CookiePrice = 44;
 let AutoAdd5CookiePrice = 169;
 let AutoAdd10CookiePrice = 690;
 let AutoAdd50CookiePrice = 1699;
-
+let AutoAdd150CookiePrice = 4999;
 
 BigCookie.onclick = function(){
   NumOfCookies += CookiePerClick;
@@ -54,7 +57,7 @@ Add1Cookie.onclick = function(){
     NumOfCookies-=Add1CookiePrice;
     setTimeout(() => {
       Add1CookiePrice+=5;
-      document.querySelector("#Add1Cookie p").textContent = Add1CookiePrice + " " + "Cookies";
+      document.querySelector("#Add1Cookie p span").textContent = Add1CookiePrice;
     }, 100);
     NumOfCookies == 1 ? CookiePreview.textContent = NumOfCookies + " " + "Cookie" : CookiePreview.textContent = NumOfCookies + " " + "Cookies";
   }
@@ -67,7 +70,7 @@ Add5Cookie.onclick = function(){
     NumOfCookies-=Add5CookiePrice;
     setTimeout(() => {
       Add5CookiePrice+=10;
-      document.querySelector("#Add5Cookie p").textContent = Add5CookiePrice + " " + "Cookies";
+      document.querySelector("#Add5Cookie p span").textContent = Add5CookiePrice;
     }, 100);
     NumOfCookies == 1 ? CookiePreview.textContent = NumOfCookies + " " + "Cookie" : CookiePreview.textContent = NumOfCookies + " " + "Cookies";
   }
@@ -80,7 +83,7 @@ Add10Cookie.onclick = function(){
     NumOfCookies-=Add10CookiePrice;
     setTimeout(() => {
       Add10CookiePrice+=50;
-      document.querySelector("#Add10Cookie p").textContent = Add10CookiePrice + " " + "Cookies";
+      document.querySelector("#Add10Cookie p span").textContent = Add10CookiePrice;
     }, 100);
     NumOfCookies == 1 ? CookiePreview.textContent = NumOfCookies + " " + "Cookie" : CookiePreview.textContent = NumOfCookies + " " + "Cookies";
   }
@@ -93,7 +96,7 @@ Add50Cookie.onclick = function(){
     NumOfCookies-=Add50CookiePrice;
     setTimeout(() => {
       Add1CookiePrice+=690;
-      document.querySelector("#Add50Cookie p").textContent = Add1CookiePrice + " " + "Cookies";
+      document.querySelector("#Add50Cookie p span").textContent = Add50CookiePrice;
     }, 100);
     NumOfCookies == 1 ? CookiePreview.textContent = NumOfCookies + " " + "Cookie" : CookiePreview.textContent = NumOfCookies + " " + "Cookies";
   }
@@ -109,7 +112,7 @@ AutoAdd1Cookie.onclick = function(){
     CookiePerSecond+=1;
     setTimeout(() => {
       AutoAdd1CookiePrice+=9;
-      document.querySelector("#AutoAdd1Cookie p").textContent = AutoAdd1CookiePrice + " " + "Cookies";
+      document.querySelector("#AutoAdd1Cookie p span").textContent = AutoAdd1CookiePrice;
     }, 100);
     NumOfCookies == 1 ? CookiePreview.textContent = NumOfCookies + " " + "Cookie" : CookiePreview.textContent = NumOfCookies + " " + "Cookies";
     cookiesPerSecondPreview.textContent = CookiePerSecond == 1 ? CookiePerSecond + " " + "Cookie per second" : CookiePerSecond + " " + "Cookies per second"
@@ -122,7 +125,7 @@ AutoAdd5Cookie.onclick = function(){
     CookiePerSecond+=5;
     setTimeout(() => {
       AutoAdd5CookiePrice+=21;
-      document.querySelector("#AutoAdd5Cookie p").textContent = AutoAdd5CookiePrice + " " + "Cookies";
+      document.querySelector("#AutoAdd5Cookie p span").textContent = AutoAdd5CookiePrice;
     }, 100);
     NumOfCookies == 1 ? CookiePreview.textContent = NumOfCookies + " " + "Cookie" : CookiePreview.textContent = NumOfCookies + " " + "Cookies";
     cookiesPerSecondPreview.textContent = CookiePerSecond == 1 ? CookiePerSecond + " " + "Cookie per second" : CookiePerSecond + " " + "Cookies per second"
@@ -134,7 +137,7 @@ AutoAdd10Cookie.onclick = function(){
     NumOfCookies-=AutoAdd10CookiePrice;
     setTimeout(() => {
       AutoAdd10CookiePrice+=69;
-      document.querySelector("#AutoAdd10Cookie p").textContent = AutoAdd10CookiePrice + " " + "Cookies";
+      document.querySelector("#AutoAdd10Cookie p span").textContent = AutoAdd10CookiePrice;
     }, 100);
     CookiePerSecond+=10;
     NumOfCookies == 1 ? CookiePreview.textContent = NumOfCookies + " " + "Cookie" : CookiePreview.textContent = NumOfCookies + " " + "Cookies";
@@ -147,9 +150,26 @@ AutoAdd50Cookie.onclick = function(){
     NumOfCookies-=AutoAdd50CookiePrice;
     setTimeout(() => {
       AutoAdd50CookiePrice+=769;
-      document.querySelector("#AutoAdd50Cookie p").textContent = AutoAdd50CookiePrice + " " + "Cookies";
+      document.querySelector("#AutoAdd50Cookie p span").textContent = AutoAdd50CookiePrice;
     }, 100);
     CookiePerSecond+=50;
+    NumOfCookies == 1 ? CookiePreview.textContent = NumOfCookies + " " + "Cookie" : CookiePreview.textContent = NumOfCookies + " " + "Cookies";
+    cookiesPerSecondPreview.textContent = CookiePerSecond == 1 ? CookiePerSecond + " " + "Cookie per second" : CookiePerSecond + " " + "Cookies per second"
+  }
+}
+setInterval(() => {
+  NumOfCookies+=CookiePerSecond;
+  CookiePreview.textContent = NumOfCookies == 1 ? CookiePreview.textContent = NumOfCookies + " " + "Cookie" : CookiePreview.textContent = NumOfCookies + " " + "Cookies";
+}, 1000);
+//Auto +150 Cookies
+AutoAdd150Cookie.onclick = function(){
+  if(NumOfCookies >= AutoAdd150CookiePrice){
+    NumOfCookies-=AutoAdd150CookiePrice;
+    setTimeout(() => {
+      AutoAdd150CookiePrice+=1169;
+      document.querySelector("#AutoAdd150Cookie p span").textContent = AutoAdd150CookiePrice;
+    }, 100);
+    CookiePerSecond+=150;
     NumOfCookies == 1 ? CookiePreview.textContent = NumOfCookies + " " + "Cookie" : CookiePreview.textContent = NumOfCookies + " " + "Cookies";
     cookiesPerSecondPreview.textContent = CookiePerSecond == 1 ? CookiePerSecond + " " + "Cookie per second" : CookiePerSecond + " " + "Cookies per second"
   }
